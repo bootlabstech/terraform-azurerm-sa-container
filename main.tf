9dba25d8e6b069682f168efbca52d70c26f35133
lalitha2407 <lalitha@bootlabstech.com>
2023-04-05 10:10:15 +0530

    initial

---
+++ main.tf
@@ -0,0 +1,10 @@
+data "azurerm_storage_account" "example" {
+  name                = var.storage_account_name
+  resource_group_name = var.resource_group_name
+  # location            = var.location
+}
+resource "azurerm_storage_container" "example" {
+  name                  = var.name
+  storage_account_name  = data.azurerm_storage_account.example.name
+  container_access_type = var.container_access_type
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "storage_account_name" {
+  type        = string
+  description = "value"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "value"
+}
+# variable "location" {
+#   type        = string
+#   description = "value"
+# }
+variable "name" {
+  type        = string
+  description = "value"
+}
+variable "container_access_type" {
+  type        = string
+  description = "value"
+}
