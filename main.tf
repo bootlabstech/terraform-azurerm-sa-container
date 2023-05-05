1f5b338d65a9e1d5f1dea44fe60a3090341f7674
lalitha2407 <lalitha@bootlabstech.com>
2023-05-05 12:54:24 +0530

    added additional config

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 data "azurerm_storage_account" "example" {
   name                = var.storage_account_name
   resource_group_name = var.resource_group_name
-  # location            = var.location
 }
 resource "azurerm_storage_container" "example" {
-  for_each= { for container in var.container_details : container.name => container }
-  name                  = each.value.name
+  name                  = var.name
   storage_account_name  = data.azurerm_storage_account.example.name
-  container_access_type = each.value.container_access_type 
+  container_access_type = var.container_access_type
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,21 @@
 variable "storage_account_name" {
   type        = string
   description = "value"
+
 }
 variable "resource_group_name" {
   type        = string
   description = "value"
-}
 
-# variable "name_of_containers" {
-#   type        = list(string)
-#   description = "value"
-# }
-# variable "container_access_type" {
-#   type        = list(string)
-#   description = "value"
-# }
-variable "container_details" {
-  type = list(any)
+}
+variable "name" {
+  type        = string
   description = "value"
-  
+
+}
+variable "container_access_type" {
+  type        = string
+  description = "value"
+
+
 }
