456ce6db97060db6d5320937485380f48ff0fe27
Bhargava Naidu <bhargava@bootlabstech.com>
2025-06-18 16:43:31 +0530

    changed the variable description and for temp

--- variables.tf
+++ variables.tf
@@ -1,13 +1,19 @@
 variable "storage_account_name" {
   type        = string
-  description = "value"
+  description = "Name of the storage account"
+
 }
 variable "resource_group_name" {
   type        = string
-  description = "value"
+  description = "provide resource group name"
+
 }
-variable "container_details" {
-  type = list(any)
-  description = "value"
-  
+variable "name" {
+  type        = string
+  description = "Name of the SA container you wish"
+
+}
+variable "container_access_type" {
+  type        = string
+  description = "provide container access type"
 }
